ac180a42d155c9323821a748e9103c7a473c57d9
Nguyên <57996971+nguyenltng@users.noreply.github.com>
2020-09-09 11:38:00 +0700

    fea4(1) - insert file 1.php

---
+++ 1.php
@@ -0,0 +1,30 @@
+<?php
+
+$employee = array(
+    array(
+        'name' => 'Nguyễn Văn Cường',
+        'blog' => 'zing.vn',
+        'position' => 'admin'
+    ),
+    array(
+        'name' => 'Trương Phúc Hoài Minh',
+        'blog' => 'vnexpress.vn',
+        'position' => 'author'
+    ),
+    array(
+        'name' => 'Hoàng Văn Tuyền',
+        'blog' => 'thanhnien.vn',
+        'position' => 'collaborators'
+    ),
+    array(
+        'name' => 'Nguyễn Tình',
+        'blog' => 'tuoitre.vn',
+        'position' => 'author'
+    )
+);
+
+$search = $employee[1];
+print_r($search)
+
+
+?>
